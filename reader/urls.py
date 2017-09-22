79bd4978f94af591006266bb83811ce8b904a567
jordanbarriap <PAWSRES1@PAWSs-MacBook-Pro.local>
2017-09-22 01:11:08 -0400

    Optimized reading progress calculation

--- reader/urls.py
+++ reader/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
         #url(r'^reader/(?P<course_id>[0-9]+)/$', views.load_course, name="course"),
-        url(r'^reader/(?P<group_id>[\w]+)/$', views.load_course, name="group"),
+        url(r'^reader/(?P<url_group_id>[\w]+)/$', views.load_course, name="group"),
         url(r'^login/$', auth_views.login, name='login'),
         url(r'^logout/$', auth_views.logout, name='logout'),
         url(r'^admin/', admin.site.urls),
--- reader/views.py
+++ reader/views.py
@@ -37,14 +37,12 @@ def load_course(request,url_group_id):
     global user_id, group_id
     group_id = url_group_id
     course_id = Group.objects.only("course").get(id=group_id).course.id
-    print(course_id)
     if request.user.is_authenticated():
-
         user_id = request.user.id
         try:
             last_page_read = ReadingLog.objects.values().filter(user__id=user_id, group__id=group_id, action="page-load").latest("datetime")
             print(connection.queries[-1]["sql"])
-            print(last_page_read["page"])
+            print(last_page_read)
             last_page_read["datetime"] = str(last_page_read["datetime"])
             last_page_read["zoom"] = float(last_page_read["zoom"])
         except ReadingLog.DoesNotExist:
@@ -52,89 +50,71 @@ def load_course(request,url_group_id):
             print("No reading")
         course_json = Course.objects.get(id=course_id)
         num_students = int(Group.objects.annotate(num_students=Count('students'))[0].num_students)
-        print(num_students)
         hierarchical_structure = course_json.course_structure
-        linear_structure = []
         calculate_reading_progress(hierarchical_structure)
-        #enrich_course_structure(linear_structure, hierarchical_structure, user_id, group_id)
-        #enrich_course_structure(linear_structure,hierarchical_structure,user_id,group_id,num_students-1)
-        return render(request, "reader.html", {"group":group_id, "course":{"id":course_json.id, "name":course_json.name}, "course_hierarchical":hierarchical_structure,"course_linear":linear_structure,"last_page_read":last_page_read})
+        return render(request, "reader.html", {"group":group_id, "course":{"id":course_json.id, "name":course_json.name}, "course_hierarchical":hierarchical_structure,"last_page_read":last_page_read})
 
-'''def enrich_course_structure(array,data,user_id,group_id):
-    if "children" in data:
-        for subsection in data["children"]:
-            #Add the id of each section
-            id = ""
-            if "id" in subsection:
-                id = subsection["id"]
-            #Add the name of each section
-            name = ""
-            if "name" in subsection:
-                name = subsection["name"]
-            #Add start and end pages information for each section
-            spage = ""
-            epage = ""
-            resourceid = ""
-            if ("spage" in subsection) and ("epage" in subsection) and ("resourceid" in subsection):
-                spage = int(subsection["spage"])
-                epage = int(subsection["epage"])
-                resourceid = subsection["resourceid"]
-                visited_pages = set(ReadingLog.objects.values_list('page', flat=True).filter(user__id=user_id, group__id=group_id, section=id, action="page-load"))
-                num_visited_pages = len(visited_pages)
-                array.append({"section": id, "name": name, "spage": spage, "epage": epage, "resource": resourceid, "visited_pages":num_visited_pages})
-                subsection["num_visited_pages"] = num_visited_pages
-            enrich_course_structure(array,subsection,user_id,group_id)'''
 
-'''def calculate_sections_num_pages(data):
-    if "children" in data:
-        for subsection in data["children"]:
-            #Add the id of each section
-            id = ""
-            if "id" in subsection:
-                id = subsection["id"]
-            #Add the name of each section
-            name = ""
-            if "name" in subsection:
-                name = subsection["name"]
-            #Add start and end pages information for each section
-            spage = ""
-            epage = ""
-            if "spage" in subsection and "epage" in subsection and "resourceid" in subsection:
-                #print("not a leaf with pages")
-                spage = int(subsection["spage"])
-                epage = int(subsection["epage"])
-                num_pages = epage - spage + 1;
-                num_pages = num_pages + calculate_sections_num_pages(subsection)
-                subsection["num_pages"] = num_pages
-                #enrich_course_structure(array, subsection, user_id, group_id, num_students)
-                #print(array)
-            else:
-                num_pages = calculate_sections_num_pages(subsection)
-                subsection["num_pages"] = num_pages
-        return num_pages
+def calculate_reading_progress(node):
+    if "children" not in node:
+        num_pages = get_number_of_pages(node)
+        read_pages = get_number_of_read_pages(node)
+        set_number_of_pages(node,num_pages)
+        set_number_of_read_pages(node, read_pages)
+        return num_pages, read_pages
     else:
-        subsection = data
-        # Add the id of each section
-        id = ""
-        if "id" in subsection:
-            id = subsection["id"]
-        # Add the name of each section
-        name = ""
-        if "name" in subsection:
-            name = subsection["name"]
-        # Add start and end pages information for each section
-        spage = ""
-        epage = ""
-        num_pages = 0
-        if "spage" in subsection and "epage" in subsection and "resourceid" in subsection:
-            # print("not a leaf with pages")
-            spage = int(subsection["spage"])
-            epage = int(subsection["epage"])
-            num_pages = epage - spage + 1;
-            subsection["num_pages"] = num_pages
-            # enrich_course_structure(array, subsection, user_id, group_id, num_students)
-            # print(array)
-            return num_pages'''
+        if has_pages(node):
+            subsections_num_pages, subsections_read_pages = calculate_subsections_reading_progress(node["children"])
+            num_pages = get_number_of_pages(node) + subsections_num_pages
+            read_pages = get_number_of_read_pages(node) + subsections_read_pages
+            set_number_of_pages(node,num_pages)
+            set_number_of_read_pages(node,read_pages)
+            return num_pages, read_pages
+        else:
+            num_pages, read_pages = calculate_subsections_reading_progress(node["children"])
+            set_number_of_pages(node,num_pages)
+            set_number_of_read_pages(node, read_pages)
+            return num_pages, read_pages
+
+
+def calculate_subsections_reading_progress(subsections):
+    total_pages = 0
+    read_pages = 0
+    for subsection in subsections:
+        subsection_total_pages, subsection_read_pages = calculate_reading_progress(subsection)
+        total_pages = total_pages + subsection_total_pages
+        read_pages = read_pages + subsection_read_pages
+    return total_pages, read_pages
+
+
+def get_number_of_pages(node):
+    spage = int(node["spage"])
+    epage = int(node["epage"])
+    return epage - spage + 1
+
+
+def get_number_of_read_pages(node):
+    global user_id, group_id
+    section_id = node["id"]
+    read_pages = set(
+        ReadingLog.objects.values_list('page', flat=True).filter(user__id=user_id, group__id=group_id, section=section_id,
+                                                                 action="page-load"))
+    return len(read_pages)
+
+
+def has_pages(node):
+    if "spage" in node and "epage" in node and "resourceid" in node:
+        return True
+    else:
+        return False
+
+
+def set_number_of_pages(node, num_pages):
+    node["num_pages"] = num_pages
+
+
+def set_number_of_read_pages(node, num_read_pages):
+    node["read_pages"] = num_read_pages
 
 
 '''def enrich_course_structure(array,data,user_id,group_id,num_students):
@@ -234,62 +214,78 @@ def load_course(request,url_group_id):
         return num_visited_pages'''
 
 
-def calculate_reading_progress(node):
-    if not node["children"]:
-        num_pages = get_number_of_pages(node)
-        read_pages = get_number_of_read_pages(node)
-        set_number_of_pages(node,num_pages)
-        set_number_of_read_pages(node, read_pages)
-        return num_pages, read_pages
+'''def enrich_course_structure(array,data,user_id,group_id):
+    if "children" in data:
+        for subsection in data["children"]:
+            #Add the id of each section
+            id = ""
+            if "id" in subsection:
+                id = subsection["id"]
+            #Add the name of each section
+            name = ""
+            if "name" in subsection:
+                name = subsection["name"]
+            #Add start and end pages information for each section
+            spage = ""
+            epage = ""
+            resourceid = ""
+            if ("spage" in subsection) and ("epage" in subsection) and ("resourceid" in subsection):
+                spage = int(subsection["spage"])
+                epage = int(subsection["epage"])
+                resourceid = subsection["resourceid"]
+                visited_pages = set(ReadingLog.objects.values_list('page', flat=True).filter(user__id=user_id, group__id=group_id, section=id, action="page-load"))
+                num_visited_pages = len(visited_pages)
+                array.append({"section": id, "name": name, "spage": spage, "epage": epage, "resource": resourceid, "visited_pages":num_visited_pages})
+                subsection["num_visited_pages"] = num_visited_pages
+            enrich_course_structure(array,subsection,user_id,group_id)'''
+
+'''def calculate_sections_num_pages(data):
+    if "children" in data:
+        for subsection in data["children"]:
+            #Add the id of each section
+            id = ""
+            if "id" in subsection:
+                id = subsection["id"]
+            #Add the name of each section
+            name = ""
+            if "name" in subsection:
+                name = subsection["name"]
+            #Add start and end pages information for each section
+            spage = ""
+            epage = ""
+            if "spage" in subsection and "epage" in subsection and "resourceid" in subsection:
+                #print("not a leaf with pages")
+                spage = int(subsection["spage"])
+                epage = int(subsection["epage"])
+                num_pages = epage - spage + 1;
+                num_pages = num_pages + calculate_sections_num_pages(subsection)
+                subsection["num_pages"] = num_pages
+                #enrich_course_structure(array, subsection, user_id, group_id, num_students)
+                #print(array)
+            else:
+                num_pages = calculate_sections_num_pages(subsection)
+                subsection["num_pages"] = num_pages
+        return num_pages
     else:
-        if has_pages(node):
-            subsections_num_pages, subsections_read_pages = calculate_subsections_reading_progress(node["children"])
-            num_pages = get_number_of_pages(node) + subsections_num_pages
-            read_pages = get_number_of_read_pages(node) + subsections_read_pages
-            set_number_of_pages(node,num_pages)
-            set_number_of_read_pages(node,read_pages)
-            return num_pages, read_pages
-        else:
-            num_pages, read_pages = calculate_subsections_reading_progress(node["children"])
-            set_number_of_pages(node,num_pages)
-            set_number_of_read_pages(node, read_pages)
-            return num_pages, read_pages
-
-
-def calculate_subsections_reading_progress(subsections):
-    total_pages = 0
-    read_pages = 0
-    for subsection in subsections:
-        total_pages = total_pages + get_number_of_pages(subsection)
-        read_pages = read_pages + get_number_of_read_pages(subsection)
-    return total_pages, read_pages
-
-
-def get_number_of_pages(node):
-    spage = int(node["spage"])
-    epage = int(node["epage"])
-    return epage - spage + 1
-
-
-def get_number_of_read_pages(node):
-    global user_id, group_id
-    section_id = node["id"]
-    read_pages = set(
-        ReadingLog.objects.values_list('page', flat=True).filter(user__id=user_id, group__id=group_id, section=section_id,
-                                                                 action="page-load"))
-    return len(read_pages)
-
-
-def has_pages(node):
-    if "spage" in node and "epage" in node and "resourceid" in node:
-        return True
-    else:
-        return False
-
-
-def set_number_of_pages(node, num_pages):
-    node["num_pages"] = num_pages
-
-
-def set_number_of_read_pages(node, num_read_pages):
-    node["read_pages"] = num_read_pages
+        subsection = data
+        # Add the id of each section
+        id = ""
+        if "id" in subsection:
+            id = subsection["id"]
+        # Add the name of each section
+        name = ""
+        if "name" in subsection:
+            name = subsection["name"]
+        # Add start and end pages information for each section
+        spage = ""
+        epage = ""
+        num_pages = 0
+        if "spage" in subsection and "epage" in subsection and "resourceid" in subsection:
+            # print("not a leaf with pages")
+            spage = int(subsection["spage"])
+            epage = int(subsection["epage"])
+            num_pages = epage - spage + 1;
+            subsection["num_pages"] = num_pages
+            # enrich_course_structure(array, subsection, user_id, group_id, num_students)
+            # print(array)
+            return num_pages'''
